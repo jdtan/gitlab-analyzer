c39715ea325982db51083c098cd351d31d1dae72
Henry Fang <fanghaof@sfu.ca>
2021-03-18 01:46:26 -0700

    Added folders for clion

---
+++ cmake-build-debug/CMakeFiles/peregrine_directed_clion.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/apps/count.cc" "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/cmake-build-debug/CMakeFiles/peregrine_directed_clion.dir/apps/count.cc.o"
+  "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/apps/existence-query.cc" "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/cmake-build-debug/CMakeFiles/peregrine_directed_clion.dir/apps/existence-query.cc.o"
+  "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/apps/fsm.cc" "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/cmake-build-debug/CMakeFiles/peregrine_directed_clion.dir/apps/fsm.cc.o"
+  "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/apps/testGraphhh.cc" "/cygdrive/e/OneDrive/SFU/SFU_Projects/Capstone_2021_Sping/peregrine-directed-clion/cmake-build-debug/CMakeFiles/peregrine_directed_clion.dir/apps/testGraphhh.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././core"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/peregrine_directed_clion.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/peregrine_directed_clion.dir/apps/count.cc.o"
+  "CMakeFiles/peregrine_directed_clion.dir/apps/existence-query.cc.o"
+  "CMakeFiles/peregrine_directed_clion.dir/apps/fsm.cc.o"
+  "CMakeFiles/peregrine_directed_clion.dir/apps/testGraphhh.cc.o"
+  "libperegrine_directed_clion.dll.a"
+  "peregrine_directed_clion.exe"
+  "peregrine_directed_clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/peregrine_directed_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
